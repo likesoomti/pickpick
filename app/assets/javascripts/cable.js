f30f28e0b9e5fb3c16e15a3408494724fb9f2b50
양효영 <>
2017-11-26 21:42:02 +0900

    cable.js

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer('ws://spacepick.org/cable');
+  App.cable = ActionCable.createConsumer('ws://localhost:3000/cable');
 
 }).call(this);
